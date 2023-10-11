508ed658f9cde69683bb68baa68054a1a83ab14a
Théotime <92151968+Pagiestm@users.noreply.github.com>
2023-10-11 21:03:12 +0200

    feat(joueur.cs, joueur.tscn, monde.tscn):animations player right, left, up and down

--- Joueur/joueur.cs
+++ Joueur/joueur.cs
@@ -5,6 +5,12 @@ public partial class joueur : CharacterBody2D
 {
 [Export]
 	public int Speed { get; set; } = 115;
+	private AnimationPlayer animations;
+	
+	public override void _Ready()
+	{
+		animations = GetNode<AnimationPlayer>("AnimationPlayer");
+	}
 
 	public void GetInput()
 	{
@@ -12,10 +18,37 @@ public partial class joueur : CharacterBody2D
 		Vector2 inputDirection = Input.GetVector("ui_left", "ui_right", "ui_up", "ui_down");
 		Velocity = inputDirection * Speed;
 	}
+	
+	public void UpdateAnimation()
+{
+	if (Velocity.Length() == 0)
+	{
+		animations.Stop();
+	}
+	else
+	{
+		string direction = "Down";
+		if (Velocity.X < 0.0f)
+		{
+			direction = "Left";
+		}
+		else if (Velocity.X > 0.0f)
+		{
+			direction = "Right";
+		}
+		else if (Velocity.Y < 0.0f)
+		{
+			direction = "Up";
+		}
+
+		animations.Play("walk" + direction);
+	}
+}
 
 	public override void _PhysicsProcess(double delta)
 	{
 		GetInput();
 		MoveAndSlide();
+		UpdateAnimation();
 	}
 }
